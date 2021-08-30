f281db215aa2c08dd4947f7f9daf922ce2a6be1a
Gregory <grihas56@mail.ru>
2021-08-30 17:45:19 +0300

    first commit

---
+++ client/src/actions/posts.js
@@ -0,0 +1,110 @@
+import * as api from '../api';
+import { COMMENT, START_LOADING, END_LOADING, FETCH_ALL, FETCH_POST, FETCH_BY_SEARCH, UPDATE, DELETE, LIKE, CREATE } from '../constants/actionTypes';
+
+// Action Creators
+
+export const getPost = (id) => async (dispatch) => {
+
+    try { 
+        dispatch({ type: START_LOADING });
+
+        const { data } = await api.fetchPost(id);
+        console.log(data);
+        dispatch({ type: FETCH_POST, payload: data });
+
+        dispatch({ type: END_LOADING });
+    } catch (error) {
+        console.log(error);
+    }
+
+};
+
+export const getPosts = (page) => async (dispatch) => {
+
+    try { 
+        dispatch({ type: START_LOADING });
+
+        const { data } = await api.fetchPosts(page);
+        console.log(data);
+        dispatch({ type: FETCH_ALL, payload: data });
+
+        dispatch({ type: END_LOADING });
+    } catch (error) {
+        console.log(error);
+    }
+
+};
+
+export const getPostsBySearch = (searchQuery) => async(dispatch) => {
+    try {
+        dispatch({ type: START_LOADING });
+        const { data: { data } } = await api.fetchPostsBySearch(searchQuery);
+
+        dispatch({ type: FETCH_BY_SEARCH, payload: data });
+
+        dispatch({ type: END_LOADING });
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+export const createPost = (post, history) => async (dispatch) => {
+    try {
+        dispatch({ type: START_LOADING });
+        const { data } = await api.createPost(post);
+
+        history.push(`/posts/${data._id}`)
+
+        dispatch({ type: CREATE, payload: data});
+
+        dispatch({ type: END_LOADING });
+        
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+        const { data } = await api.updatePost(id, post);
+
+        dispatch({ type: UPDATE, payload: data});
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id);
+
+        dispatch({type: DELETE, payload: id});
+    } catch (error) {
+        console.log(error);
+    }
+
+};
+
+export const likePost = (id) => async (dispatch) => {
+    try {
+        const { data } = await api.likePost(id);
+
+        dispatch({ type: LIKE, payload: data});
+    } catch (error) {
+        console.log(error);
+    }
+
+};
+
+export const commentPost = (value, id) => async (dispatch) => {
+    try {
+        const { data } = await api.comment(value, id);
+        
+        dispatch({ type: COMMENT, payload: data });
+
+        return data.comments; // we returnin nearest comment, that is coming in
+    } catch (error) {
+        console.log(error);
+    }
+}
